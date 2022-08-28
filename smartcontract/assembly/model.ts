6b55055735dd0e827ed663bfe655f14023d1d1b9
Farzeen Kistnareddy <90398313+FarzeenKist@users.noreply.github.com>
2022-08-28 10:14:26 +0400

    Improvements and fixes

--- smartcontract/assembly/model.ts
+++ smartcontract/assembly/model.ts
@@ -12,7 +12,9 @@ export class Item {
     owner: string;
     forSale: bool;
     forExchange: bool;
-
+    requestExchangeId: string; // id of the item a user wants to trade the current item with
+    approvedExchange: bool; // boolean to keep track of whether requestExchangeId has been accepted by the item owner
+    approvedId: string; // agreed item's id to exchange with
     public static fromPayload(payload: Item): Item {
         const item = new Item();
         item.id = payload.id;
@@ -27,10 +29,59 @@ export class Item {
         return item;
     }
     
+    public buyItem(): void {
+        this.forSale = false;
+        this.forExchange = false;
+        this.owner = context.sender;
+    }
+
+    public rejectExchange(): void {
+        this.requestExchangeId = "";
+        this.approvedExchange = false;
+        this.approvedId = "";
+    }
+    
+    public requestExchange(itemId: string): void {
+        this.requestExchangeId = itemId;
+        this.forExchange = false; // to prevent override of the first request for exchange
+    }
+    // locks an item for an exchange after request an exchanging offer
+    public lockForExchange(itemId: string): void {
+        this.forExchange = false;
+        this.requestExchangeId = "";
+        this.approvedExchange = true;
+        this.approvedId = itemId;
+    }
+    
+    /**
+     * 
+     * @returns bool whether item can have request for trades/toggle exchange status/ toggle sale status
+     */
+    public canBeExchanged(): bool {
+        if(this.requestExchangeId != "" || this.approvedId != ""){
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+    public exchange(owner: string): void {
+        this.approvedExchange = false;
+        this.approvedId = "";
+        this.forSale = false;
+        this.owner = owner;
+    }
+
+    public toggleForSale(): void {
+        this.forSale = !this.forSale;
+    }
+
+    public toggleForExchange(): void {
+        this.forExchange = !this.forExchange;
+    }
 
 }
 
 
 
 export const itemsStorage = new PersistentUnorderedMap<string, Item>("LISTED_ITEMS");
-export const ownersStorage = new PersistentUnorderedMap<string, bool>("LISTED_OWNERS");
